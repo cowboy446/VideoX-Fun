ad2be1ad92e0eea68719bc1a89cbb13d18ae2399
zhangrong <3174848262@qq.com>
2026-04-10 06:32:14 +0000

    update train config v1 for ti2v 5b model

--- scripts_mousegen_wan22/wan22_train.sh
+++ scripts_mousegen_wan22/wan22_train.sh
@@ -1,12 +1,14 @@
 export MODEL_NAME="models/wan22/Wan2.2-TI2V-5B"
-export DATASET_NAME="datasets/internal_datasets/"
-export DATASET_META_NAME="datasets/internal_datasets/metadata.json"
+export DATASET_NAME="datasets/poor_dataset_01/"
+export DATASET_META_NAME="datasets/poor_dataset_01/meta_text_video_pairs_qwen_8b.json"
+export CUDA_VISIBLE_DEVICES=0,4,5,7
+export WAN22_ACCELERATE="/home/zhangrong/miniconda3/envs/wan22/bin/accelerate"
 # NCCL_IB_DISABLE=1 and NCCL_P2P_DISABLE=1 are used in multi nodes without RDMA. 
 # export NCCL_IB_DISABLE=1
 # export NCCL_P2P_DISABLE=1
 NCCL_DEBUG=INFO
 
-accelerate launch --mixed_precision="bf16" scripts_mousegen_wan22/wan22_train.py \
+"$WAN22_ACCELERATE" launch --mixed_precision="bf16" scripts_mousegen_wan22/wan22_train.py \
   --config_path="config/wan2.2/wan_civitai_5b.yaml" \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --train_data_dir=$DATASET_NAME \
@@ -17,11 +19,11 @@ accelerate launch --mixed_precision="bf16" scripts_mousegen_wan22/wan22_train.py
   --video_sample_stride=1 \
   --video_sample_n_frames=81 \
   --train_batch_size=1 \
-  --video_repeat=1 \
+  --video_repeat=0 \
   --gradient_accumulation_steps=4 \
   --dataloader_num_workers=8 \
   --num_train_epochs=100 \
-  --checkpointing_steps=20 \
+  --checkpointing_steps=3000 \
   --learning_rate=2e-05 \
   --lr_scheduler="constant_with_warmup" \
   --lr_warmup_steps=100 \
@@ -38,10 +40,17 @@ accelerate launch --mixed_precision="bf16" scripts_mousegen_wan22/wan22_train.py
   --enable_bucket \
   --uniform_sampling \
   --low_vram \
-  --boundary_type="low" \
-  --train_mode="normal" \
+  --boundary_type="full" \
+  --train_mode="ti2v" \
   --trainable_modules "." \
-  --validation_prompts "一只灰色小鼠在实验台上探索，自由地移动，背景是实验室环境，光线明亮，细节丰富，高清晰度" \
+  --validation_steps=300 \
+  --validation_prompts \
+    "一只黑色小鼠在实验台上探索，自由地移动，背景是实验室环境，光线明亮，细节丰富，高清晰度" \
+    "一只黑色小鼠在野外草地环境下爬行，画面生动，高清晰度" \
+  --validation_paths \
+    "datasets/validset/view4_f00637.jpg" \
+    "datasets/validset/vace_mouse_frame_001.jpg"
+
 
 # The Training Shell Code for Image to Video
 # You need to use "config/wan2.2/wan_civitai_i2v.yaml"
